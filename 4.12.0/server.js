157755a054d3d96857608740e2ba94d97985eb66
Vishal Turi <turivishal@gmail.com>
2022-11-20 19:08:09 +0530

    Initial Commit

---
+++ 4.12.0/server.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri = "mongodb://localhost:27017/?maxPoolSize=20&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    console.log("mongodb-npm-4.12.0");
+    console.log("==================");
+    console.time("connection");
+    await client.connect();
+    console.timeEnd("connection");
+    console.time("find");
+    let c = await client.db("db_name").collection("coll_name").find().explain("executionStats");
+    console.timeEnd("find");
+    console.log("explain.executionTimeMillis: ", c.executionStats.executionTimeMillis);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # node-mongodb-native-performance
 Test difference versions of node-mongodb-native driver
+
+## Execute the server:
+- Go specific directory from 4.3.0, 4.6.0, 4.12.0
+```
+cd 4.3.0
+```
+- NPM install
+```
+npm install
+```
+- Run the server file
+```
+node server.js
+```
+
+## Result:
+```
+mongodb-npm-4.3.0
+=================
+connection: 15.517ms
+find: 10.751ms
+explain.executionTimeMillis:  0
+```
